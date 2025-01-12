ba24aef4d378802cedf818c0c35fe4fb9ea85e57
Alexandre Teixeira <alextp@gmail.com>
2025-01-12 01:32:29 -0300

    Criação dos componentes Botao, Formulario e Lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Formulario from './componentes/Formulario';
+import Lista from './componentes/Lista';
 
 function App() {
   return (
     <div className="App">
-Hello World
+      <Formulario />
+      <Lista />
     </div>
   );
 }
