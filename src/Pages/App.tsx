fc9e66496386aadc72b7235197403031b8790672
Alexandre Teixeira <alextp@gmail.com>
2025-01-17 01:38:42 -0300

    implementação do cronometro

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
+import Cronometro from '../componentes/Cronometro';
 import Formulario from '../componentes/Formulario';
 import Lista from '../componentes/Lista';
-import style from './App.module.scss';
-import Cronometro from '../componentes/Cronometro';
 import ITarefa from '../types/tarefa';
+import style from './App.module.scss';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
 
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
   return (
     <div className={style.AppStyle}>
-      <Formulario setTarefas={setTarefas}   />
-      <Lista tarefas={tarefas}/>
-      <Cronometro />
+      <Formulario setTarefas={setTarefas} />
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
+      <Cronometro selecionado={selecionado} />
     </div>
   );
 }
